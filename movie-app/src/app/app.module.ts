1b937d7090daeb7bec954b4001ded6be5c9e488e
sigrung15 <sigrung15@ru.is>
2017-12-27 00:34:12 +0100

    added animation to the list and also a remove button

--- movie-app/src/app/app.module.ts
+++ movie-app/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { MoviesComponent } from './movies/movies.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- movie-app/src/app/movies/movies.component.ts
+++ movie-app/src/app/movies/movies.component.ts
@@ -1,10 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
+  animations: [
+    trigger('movies', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0}), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35%)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1})
+          ]))
+        ]), {optional: true}),
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35%)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
+          ]))
+        ]), {optional: true})
+      ])
+    ])
+  ]
 })
+
 export class MoviesComponent implements OnInit {
 
   itemCount: number;
@@ -29,6 +52,10 @@ export class MoviesComponent implements OnInit {
     this.movieText = '';
     this.movieLink = '';
     this.itemCount = this.movies.length;
+  }
+
+  removeMovie(movie) {
+    this.movies.splice(movie, 1);
   }
 
 }
