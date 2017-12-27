4896c8d2cf380aa6d06f599b108a3a5804c554f2
sigrung15 <sigrung15@ru.is>
2017-12-27 02:28:59 +0100

    plz virkaðu

--- movie-app/src/app/app.component.ts
+++ movie-app/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +7,4 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'our movie list <3';
-
-  constructor(private router: Router) { }
-
-  openMovies() {
-    this.router.navigateByUrl('');
-  }
-
-  openAbout() {
-    this.router.navigateByUrl('about')
-  }
 }
